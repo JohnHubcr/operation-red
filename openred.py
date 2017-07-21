3da2fb115728f8f002edff5a01b50838ef3bcc0b
EdOverflow <edoverflow@users.noreply.github.com>
2017-07-21 16:38:34 +0100

    Add more flags.

--- openred.py
+++ openred.py
@@ -1,54 +1,101 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-# ----------------------------------------------------------------------
+#----------------------------------------------------------------------
 # Operation Red by EdOverflow
+# Twitter: https://twitter.com/EdOverflow
+# GitHub:  https://github.com/EdOverflow
 #
 # TODO:
-# - Scan params
-# - Allow user to specify payload(s)
-# - Add params (/?continue=payload)
+# - Scan params.
+# + Allow user to specify payload(s).
+# + Add params (/?continue=payload).
 # - Replace IP payloads with correct IP.
-# ----------------------------------------------------------------------
+# - Add random user agents.
+# - Fix the detection mechanism.
+# - Print logo.
+#----------------------------------------------------------------------
 
+# Modules
 import requests
 import argparse
 import colorama
 import re
+import os
+# Python 2 & 3 require different
+# urlparse modules
 try:
     from urllib import parse as urlparse
-except ImportError:
+except ImportError as e:
     import urlparse
 
 
-# Variables
-with open('payloads.txt') as f:
-    redirect = f.read().splitlines()
+def version():
+    '''
+    Return version of Operatio Red.
+    '''
+    version = 'v.1.0.0'
+    return(version)
 
+
+def logo():
+    '''
+    Operation Red logo.
+    '''
+    logo = '''
+    Operation Red ''' + version() + '''\n
+    Author: EdOverflow
+    '''
+    return(logo)
+
+
+# Argument Parser
 parser = argparse.ArgumentParser(
-    description="Open redirect vulnerability scanner.")
+    description='A simple Python open redirect vulnerability scanner.'
+)
 parser.add_argument("-u", "--url", dest="url",
                     help="Scan individual URL for open \
                     redirect vulnerabilities")
 parser.add_argument("-t", "--txt", dest="txt",
                     help="Scan text file for open \
                     redirect vulnerabilities")
+parser.add_argument("-p", "--payloads", dest="payloads",
+                    help="Specify payload list to use \
+                    during scan.", default="payloads.txt")
+parser.add_argument("-m", "--mypayload", dest="mypayload",
+                    help="Specifiy your own payload.")
+parser.add_argument("-c", "--common", dest="params",
+                    help="Brute force commonly vulnerable \
+                    parameters and then test for open redirect\
+                    issues.")
 args = parser.parse_args()
 
 
-class symbols:
-    """
-    Standard symbols list.
-    """
+# User supplied payload.
+if args.mypayload:
+    redirect = args.mypayload
+else:
+    # Payload list.
+    with open(args.payloads, 'r') as f:
+        redirect = f.read().splitlines()
 
+
+class symbols:
+    '''
+    Standard symbols list.
+    [!] represents an error.
+    [$] is for good news!
+    [-] is for bad news.
+    '''
     error = colorama.Fore.RED + '[!] ' + colorama.Fore.RESET
     success = colorama.Fore.GREEN + '[$] ' + colorama.Fore.RESET
     negative = colorama.Fore.YELLOW + '[-] ' + colorama.Fore.RESET
+    info = colorama.Fore.BLUE + '[i] ' + colorama.Fore.RESET
 
 
 def traverse(o, tree_types=(list, tuple)):
     '''
     Iterates over all list values.
+    This function is used whenever a list is being used.
     '''
     if isinstance(o, tree_types):
         for value in o:
@@ -62,7 +109,6 @@ def url_clean(self, url):
     """
     Cleans up URLs.
     """
-
     url = url.split('?')[0]
     url = url.replace('#', '%23')
     url = url.replace(' ', '%20')
@@ -72,8 +118,10 @@ def url_clean(self, url):
 def url_handler(url):
     """
     Parses URLs.
+    Makes sure that the URL is in one of the following formats:
+    (Without parameter) http://example.com/
+    (With parameter)    http://example.com/?url=
     """
-
     try:
         # Scheme: http://
         default_protocol = 'http'
@@ -84,21 +132,37 @@ def url_handler(url):
         # Domain: example.com
         domain = urlparse.urlparse(url).netloc
 
-        # Site: http://example.com
+        # Site: http://example.com/
         site = scheme + '://' + domain
         if site.endswith('/') == False:
             site += '/'
 
-    except IndexError:
-        pass
+        if args.params:
+            '''
+            Tests commonly vulnerable parameters.
+            The paramater list can be specified using -c/--common.
+            common.txt is Operation Red's ready-made list.
+            $ python openred.py -u http://example.com -c common.txt
+            '''
+            with open(args.params, 'r') as f:
+                common_params = f.read().splitlines()
 
-    return site
+            for p in traverse(common_params):
+                # Param: /?url=
+                param = site + '?' + repr(p).replace("'", "") + '='
+                return(param)
+        else:
+            return(site)
+
+    except IndexError as e:
+        pass
 
 
 def scan(domains):
     '''
     Scans target, iterates over payloads list and
-    checks whether the URL redirects to example.com.
+    checks whether the URL redirects to the payload's
+    location.
     '''
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
@@ -112,6 +176,7 @@ def scan(domains):
         strip = repr(domains).replace("'", "")
         target = url_handler(strip) + repr(n).replace("'", "")
         print("Scanning: " + target)
+        # Send request to target.
         resp = requests.get(target,
                             headers=headers,
                             timeout=5,
@@ -120,54 +185,87 @@ def scan(domains):
         status = resp.status_code
         no_results = 'No open redirect found.'
         if resp.history:
-            if resp.url.startswith("http://example.com"):
-                print("=" * 69)
+            if args.mypayload:
+                loc = args.maypayload
+            # Open redirect found.
+            elif args.payloads:
+                loc = 'http://example.com'
+            if resp.url.startswith(loc):
+                times = 69 # Hehehe
+                print("=" * times)
                 for r in resp.history:
-                    print(symbols.success + "URL is vulnerable to open redirects!")
+                    print(symbols.success + "URL is vulnerable to \
+                        open redirects!")
                     print(str(resp.status_code) + ": " + r.url)
                     print("Final destination:")
                     print(str(resp.status_code) + ": " + resp.url)
-                    print("=" * 69)
+                    print("=" * times)
             else:
                 pass
+        # No open redirect found.
         else:
             print(symbols.negative + no_results)
 
-if args.url:
+
+def main():
     '''
-    Scans a single URL.
+    Main function.
+    - Windows colorama support.
+    - Print Operation Red logo.
     '''
-    try:
-        domains = args.url
-        scan(domains)
-    except requests.exceptions.ConnectionError:
-        print(symbols.error + args.url + ": Connection refused!")
-        pass
-    except requests.exceptions.Timeout:
-        print(symbols.error + args.url + ": Connection timed out!")
-        pass
-    except requests.exceptions.InvalidURL as e:
-        print(symbols.error + args.url + ": Invalid URL!")
-        pass
-    except requests.exceptions.TooManyRedirects:
-        print(symbols.error + "Bad URL!")
-elif args.txt:
-    '''
-    Scans a list of targets from a
-    text file.
-    '''
-    with open(args.txt) as f:
-        domains = f.read().splitlines()
-    for i in traverse(domains):
+    # This allows colorama to run
+    # on Windows.
+    colorama.init(autoreset=True)
+    logo()
+
+    if args.url:
+        '''
+        Scans a single URL.
+        $ python openred.py -u http://example.com/
+        '''
         try:
-            scan(i)
+            domains = args.url
+            scan(domains)
         except requests.exceptions.ConnectionError as e:
-            continue
+            print(symbols.error + args.url + ": Connection refused!")
+            pass
         except requests.exceptions.Timeout as e:
-            continue
+            print(symbols.error + args.url + ": Connection timed out!")
+            pass
         except requests.exceptions.InvalidURL as e:
-            continue
-        except requests.exceptions.TooManyRedirects:
-            print(symbols.error + "Bad URL!")
-else:
-    parser.print_help()
+            print(symbols.error + args.url + ": Invalid URL!")
+            pass
+        except requests.exceptions.TooManyRedirects as e:
+            print(symbols.error + "Bad URL! Too many\
+                    redirects.")
+    elif args.txt:
+        '''
+        Scans a list of targets from a
+        text file.
+        $ python -t targets.txt
+        '''
+        with open(args.txt, 'r') as f:
+            domains = f.read().splitlines()
+        for i in traverse(domains):
+            try:
+                scan(i)
+            except requests.exceptions.ConnectionError as e:
+                continue
+            except requests.exceptions.Timeout as e:
+                continue
+            except requests.exceptions.InvalidURL as e:
+                continue
+            except requests.exceptions.TooManyRedirects:
+                print(symbols.error + "Bad URL! Too many\
+                        redirects.")
+    else:
+        parser.print_help()
+
+
+if (__name__ == '__main__'):
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        print('\n' + symbols.info + 'KeyboardInterrupt detected.')
+        os._exit(1)
+        
